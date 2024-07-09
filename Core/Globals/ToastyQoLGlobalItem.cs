3beac4d79462faf0a22ecb52d2f58caddbaacfc4
Midnight295 <143055111+Midnight295@users.noreply.github.com>
2024-07-09 11:23:14 -0700

    spacer suggestion

---
+++ Core/Globals/ToastyQoLGlobalItem.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ToastyQoL.Core.Globals
+{
+    internal class ToastyQoLGlobalItem : GlobalItem
+    {
+        public override bool InstancePerEntity => true;
+
+        
+
+        public override void UpdateInventory(Item item, Player player)
+        {
+           if (Toggles.InfinitePotions)
+            {
+               if (item.type == ItemID.Campfire)
+                  player.AddBuff(BuffID.Campfire, 2);
+
+               if (item.type == ItemID.HeartLantern)
+                  player.AddBuff(BuffID.HeartLamp, 2);
+
+               if (item.type == ItemID.StarinaBottle)
+                  player.AddBuff(BuffID.StarInBottle, 2);
+            }
+               
+           
+        }
+    }
+}
